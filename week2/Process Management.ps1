ca9014398adabb6228022ad3bbefa609e8483b46
Timothy <timothy.price@mymail.champlain.edu>
2024-02-02 10:38:51 -0500

    Commit for process management lab

---
+++ week2/Process Management.ps1
@@ -0,0 +1,17 @@
+﻿clear
+
+Get-Process | Where-Object { $_.Name -ilike "c*" }
+
+Get-Process | Where-Object { $_.Path -notmatch "system32" } | Select-Object ProcessName, Path | Format-Table `
+-AutoSize -Wrap
+
+Get-Service | Where-Object { $_.Status -eq "Stopped" }
+
+if ($running -eq $true){
+    try {Stop-Process -Name "chrome"} catch {Write-Output "Chrome Instance Already Closed"}
+    $running = $false
+}
+else{
+    Start-Process -FilePath "chrome" '-ArgumentList --start-fullscreen "https://www.champlain.edu"'
+    $running = $true
+}
