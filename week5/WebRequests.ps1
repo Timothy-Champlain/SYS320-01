728973d05e74c45f5a275f6bcca9d135de60fbd4
Timothy <timothy.price@mymail.champlain.edu>
2024-02-16 11:01:20 -0500

    Windows HTML Class Activity

---
+++ week5/WebRequests.ps1
@@ -0,0 +1,23 @@
+﻿clear
+
+$scraped_page = (Invoke-WebRequest -Uri http://10.0.17.36/ToBeScraped.html)
+
+# Get a count of the links in the page
+# $scraped_page.Links.count
+
+
+# Display links as HTML Element
+# $scraped_page.Links
+
+# Display links as HTML Element
+# $scraped_page.Links | Select-Object 'outertext','href'
+
+$h2s=$scraped_page.ParsedHtml.body.getElementsByTagName('h2') | Select-Object 'outertext'
+
+# $h2s
+
+# Print innerText of every div element that has the class as "div-1"
+$divs1=$scraped_page.ParsedHtml.body.getElementsByTagName("div") | where { `
+$_.getAttributeNode("class").Value -ilike "div-1"} | select innerText
+
+$divs1
