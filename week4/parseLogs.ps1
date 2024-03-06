8cccfd2d194541ece4770cd2e20531849c5a1f35
Timothy <timothy.price@mymail.champlain.edu>
2024-03-06 15:54:18 -0500

    did parsing apache logs

---
+++ week4/parseLogs.ps1
@@ -0,0 +1,20 @@
+﻿Clear
+
+function Apachelogs1(){
+$logsNotformatted = Get-Content C:\xampp\apache\logs\access.log
+$tableRecords = @()
+for($i=0; $i -lt $logsNotformatted.count; $i++){
+$words = $logsNotformatted[$i].Split(" ");
+ $tableRecords += [pscustomobject]@{ "IP" = $words[0]; `
+                                     "Time" = $words[3].Trim('['); `
+                                     "Method" = $words[5].Trim('"'); `
+                                     "Page" = $words[6]; `
+                                     "Protocol" = $words[7]; `
+                                     "Response" = $words[8]; `
+                                     "Referrer" = $words[10]; ` }
+                                     #"Client" = $words[11..($words.
+}
+return $tableRecords | Where-Object { $_.IP -ilike "10.*" }
+}
+$tableRecords = Apachelogs1
+$tableRecords | Format-Table -AutoSize -Wrap
---
+++ week8(midterm)/midterm.ps1
@@ -0,0 +1,38 @@
+﻿clear
+
+function obtainIOC(){
+$page = Invoke-WebRequest -TimeoutSec 2 http://10.0.17.5/IOC.html
+
+$trs=$page.ParsedHtml.body.getElementsByTagName("tr")
+
+$FullTable = @()
+for($i-1; $i -lt $trs.length; $i++){
+    $tds = $trs[$i].getElementsByTagName("td")
+    $FullTable += [pscustomobject]@{"Pattern" = $tds[0].innerText; `
+                                    "Explanation" = $tds[1].innerText;
+                                   }
+}
+return $FullTable
+}
+# obtainIOC
+
+function apacheAccessLogs(){
+$logsNotformatted = Get-Content C:\Users\champuser\Downloads\access.log
+$tableRecords = @()
+for($i=0; $i -lt $logsNotformatted.count; $i++){
+    $words = $logsNotformatted[$i].Split(" ");
+    $tableRecords += [pscustomobject]@{ "IP" = $words[0]; `
+                                     "Time" = $words[3].Trim('['); `
+                                     "Method" = $words[5].Trim('"'); `
+                                     "Page" = $words[6]; `
+                                     "Protocol" = $words[7]; `
+                                     "Response" = $words[8]; `
+                                     "Referrer" = $words[10]; ` }
+}
+return $tableRecords | Where-Object { $_.IP -ilike "10.*" }
+}
+$tableRecord = apacheAccessLogs
+# $tableRecords | Format-Table -AutoSize -Wrap
+
+function IOCLogs($FullTable, $tableRecord){
+
